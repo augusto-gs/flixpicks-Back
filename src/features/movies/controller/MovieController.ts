9ef3a8fdc82797785e611d0b137aca1b4a46bc57
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-28 10:17:32 +0100

    Add MovieController and its getMovies method

---
+++ src/features/movies/controller/MovieController.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response } from "express";
+import type MovieMongooseRepository from "../repository/MoviesMongooseRepository";
+
+class MovieController {
+  constructor(public moviesRepository: MovieMongooseRepository) {}
+
+  getMovies = async (_req: Request, res: Response): Promise<void> => {
+    const movies = await this.moviesRepository.getMovies();
+    res.status(200).json({ movies });
+  };
+}
+
+export default MovieController;
